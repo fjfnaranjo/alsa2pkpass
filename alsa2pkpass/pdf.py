fd76248bf2c0fc76366bf839595828d9fe4ee1bf
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2022-12-30 11:53:02 +0100

    Consider station addresses in tickets not following travel times

--- alsa2pkpass/pdf.py
+++ alsa2pkpass/pdf.py
@@ -13,17 +13,18 @@ def parse_ticket_page(page):
     number_pattern = compile(r"^(\d+)$")
     service_pattern = compile(r"^Línea: (.*)$")
 
-    capture_origin = False
-    capture_origin_address = False
-    capture_destination = False
-    capture_destination_address = False
-    last_block_size = None
-    last_block = []
     date_captures = []
     time_captures = []
+    capture_blocks = False
     capture_numbers = False
     number_captures = []
     capture_localizer = False
+    capture_origin = False
+    capture_destination = False
+    last_block_size = None
+    last_block_x0 = None
+    last_block_x1 = None
+    block_captures = []
 
     for element in page:
         if isinstance(element, LTTextBoxHorizontal):
@@ -33,43 +34,12 @@ def parse_ticket_page(page):
             number_match = number_pattern.fullmatch(element_text)
             service_match = service_pattern.fullmatch(element_text)
 
-            if any(
-                [
-                    capture_origin,
-                    capture_origin_address,
-                    capture_destination,
-                    capture_destination_address,
-                ]
-            ):
-                if last_block_size is None:
-                    last_block_size = element.height
-                    last_block.append(element_text)
-                elif last_block_size == element.height:
-                    last_block.append(element_text)
-                else:
-                    block_as_paragraph = "\n".join(last_block)
-                    last_block_size = None
-                    last_block = []
-                    if capture_origin:
-                        capture_origin = False
-                        capture_origin_address = True
-                        ticket_data["start_city"] = block_as_paragraph
-                    elif capture_origin_address:
-                        capture_origin_address = False
-                        ticket_data["start_address"] = block_as_paragraph
-                    elif capture_destination:
-                        capture_destination = False
-                        capture_destination_address = True
-                        ticket_data["end_city"] = block_as_paragraph
-                    elif capture_destination_address:
-                        capture_destination_address = False
-                        ticket_data["end_address"] = block_as_paragraph
-
-            elif date_match is not None:
+            if date_match is not None:
                 date_captures.append(date_match.groups())
 
             elif time_match is not None:
                 time_captures.append(time_match.string)
+                capture_blocks = True
                 if "start_city" not in ticket_data:
                     capture_origin = True
                 elif "end_city" not in ticket_data:
@@ -91,6 +61,38 @@ def parse_ticket_page(page):
                 capture_localizer = False
                 ticket_data["localizer"] = element_text
 
+            elif capture_blocks:
+                if element_text in ["Clase", "Autobús"]:
+                    capture_blocks = False
+
+                else:
+                    if last_block_size is None:
+                        last_block_size = element.height
+                        last_block_x0 = element.x0
+                        last_block_x1 = element.x1 // 2
+                        block_captures.append([element_text])
+                    elif last_block_size == element.height and (
+                        last_block_x0 == element.x0 or last_block_x1 == element.x1 // 2
+                    ):
+                        block_captures[-1].append(element_text)
+                    else:
+                        last_block_size = element.height
+                        last_block_x0 = element.x0
+                        last_block_x1 = element.x1 // 2
+                        block_captures.append([element_text])
+
+                    if capture_origin or capture_destination:
+                        last_block_as_paragraph = "\n".join(block_captures.pop())
+                        last_block_size = None
+                        last_block_x0 = None
+                        last_block_x1 = None
+                        if capture_origin:
+                            capture_origin = False
+                            ticket_data["start_city"] = last_block_as_paragraph
+                        elif capture_destination:
+                            capture_destination = False
+                            ticket_data["end_city"] = last_block_as_paragraph
+
     ticket_data["start_date"], ticket_data["end_date"], *_ = date_captures
     ticket_data["start_time"], ticket_data["end_time"], *_ = time_captures
 
@@ -98,6 +100,10 @@ def parse_ticket_page(page):
         ticket_data["bus"] = number_captures[0]
     else:
         ticket_data["bus"], ticket_data["seat"], *_ = number_captures
+
+    ticket_data["start_address"], ticket_data["end_address"], *_ = [
+        "\n".join(capture) for capture in block_captures
+    ]
 
     for field in [
         "service",
